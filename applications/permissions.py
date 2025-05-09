aecf5b18a1c6cc0a1cabc0303b4b02bc2a068560
Murat99010 <Murat99010@gmail.com>
2025-05-09 22:44:42 +0500

    perms

--- applications/permissions.py
+++ applications/permissions.py
@@ -5,6 +5,8 @@ from applications.constants import StatusChoices
 
 class ApplicationEditPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
         if obj.status == StatusChoices.DRAFT:
             return obj.owner == request.user or request.user.is_staff
         return request.user.is_staff
