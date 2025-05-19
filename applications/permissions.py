c89dee80c02a5cf5a1482bdaa2b525d71d059479
Murat99010 <Murat99010@gmail.com>
2025-05-20 01:49:51 +0500

    WIP

--- applications/permissions.py
+++ applications/permissions.py
@@ -7,6 +7,6 @@ class ApplicationEditPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        if obj.status == StatusChoices.DRAFT:
-            return obj.owner == request.user or request.user.is_staff
+        if obj.status in [StatusChoices.DRAFT, StatusChoices.FOR_REVISION]:
+            return obj.owner == request.user
         return request.user.is_staff
